80277acf8f9d96a43d9b61e96d4f6ba2e2982c27
revmat56 <62846638+revmat56@users.noreply.github.com>
2020-12-09 23:08:53 +0100

    Update foo.hpp
    test1

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -8,5 +8,33 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     // Twoja implementacja tutaj
-    return {};
+    
+    std::vector<char> wektor (people.size()); //stworzenie pustego wektora obiektow typu char 
+
+    for (auto it = people.begin(); it != people.end(); ++it)
+    {
+        it -> birthday();
+        
+    } // petla aktywuje metode birthday dla wszystkich pol age podanej listy
+
+    int i = 0;
+    for (auto it = people.end(); it != people.begin();)
+    {
+      
+        --it;
+        if (it->isMonster() == true)
+        {
+            wektor[i]='n';
+        }
+        else
+        {
+            wektor[i]='y';
+        }
+
+        ++i;
+
+    } // petla do zwracanego wektora wpisuje znaki y lub n w zaleznosci od tego co wskaze is Monster, lista odczytuje sie od tylu
+
+    
+    return wektor;
 }
